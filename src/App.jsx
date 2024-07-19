8673cca188a1e6147c06b8eebfa7d15b870537e1
Rashid <rashid.yaqoob@axelerant.com>
2024-07-19 12:12:36 +0530

    LDT-563: Custom useFetch hook exercise

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import UserCard from "./components/UserCard";
 import { UserClassCard } from "./components/UserClassCard";
 import NameCounter from "./components/NameCounter";
 import { NameCounterClass } from "./components/NameCounterClass";
+import CustomHook from "./components/CustomHook";
 
 function App() {
   console.log(user);
@@ -31,6 +32,10 @@ function App() {
       <br />
 
       <NameCounterClass />
+
+      <br />
+
+      <CustomHook />
     </div>
   );
 }
