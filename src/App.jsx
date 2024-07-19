5e5a5a0d6e6f068b47c2995e67b5361423685257
Rashid <rashid.yaqoob@axelerant.com>
2024-07-19 10:34:32 +0530

    LDT-561: UserList exercise

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import UserCard from "./components/UserCard";
 import { UserClassCard } from "./components/UserClassCard";
 import NameCounter from "./components/NameCounter";
 import { NameCounterClass } from "./components/NameCounterClass";
+import UserList from "./components/UserList";
 
 function App() {
   console.log(user);
@@ -31,6 +32,10 @@ function App() {
       <br />
 
       <NameCounterClass />
+
+      <br />
+
+      <UserList />
     </div>
   );
 }
