18281556b8997788a84833f7aeb2d275502e01af
Rashid <rashid.yaqoob@axelerant.com>
2024-07-24 12:03:04 +0530

    LDT-589: Control props exercise

--- src/components/StateReducer.jsx
+++ src/components/StateReducer.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect, useState } from "react";
 import "./StateReducer.css";
 
 const initialState = {
@@ -7,14 +7,13 @@ const initialState = {
   warningMessage: "",
 };
 
-// Define the reducer function
 const reducer = (state, action) => {
-  console.log(state);
   switch (action.type) {
     case "increment":
-      if (state.count + 1 > 4) {
+      if (state.count + 1 >= 5) {
         return {
           ...state,
+          count: state.count + 1,
           isDisabled: true,
           warningMessage: "Whoa! You overclicked the button",
         };
@@ -23,31 +22,47 @@ const reducer = (state, action) => {
     case "reset":
       return initialState;
     default:
-      return initialState;
+      return state;
   }
 };
 
 const StateReducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    setToggle(state.count % 2 !== 0);
+  }, [state.count]);
 
   return (
-    <div className="switch">
-      <input
-        type="checkbox"
-        id="toggle"
-        className="switch-input"
-        disabled={state.isDisabled}
-      />
-      <label
-        htmlFor="toggle"
-        className="switch-label"
-        onClick={() => dispatch({ type: "increment" })}
-      ></label>
+    <>
+      <div className="switch">
+        <input
+          type="checkbox"
+          id="toggle1"
+          className="switch-input"
+          checked={toggle}
+          disabled={state.isDisabled}
+          onChange={() => dispatch({ type: "increment" })}
+        />
+        <label htmlFor="toggle1" className="switch-label"></label>
+      </div>
+      <div className="switch">
+        <input
+          type="checkbox"
+          id="toggle2"
+          className="switch-input"
+          checked={toggle}
+          disabled={state.isDisabled}
+          onChange={() => dispatch({ type: "increment" })}
+        />
+        <label htmlFor="toggle2" className="switch-label"></label>
+      </div>
       <p>{state.warningMessage}</p>
       {state.isDisabled && (
         <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
       )}
-    </div>
+    </>
   );
 };
 
