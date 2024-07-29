f228e353f71e52272bca87f24c37be4fbe456d47
Rashid <rashid.yaqoob@axelerant.com>
2024-07-29 14:54:16 +0530

    LDT-582: Autogrowing area exercise

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,13 @@ import UserCard from "./components/UserCard";
 import { UserClassCard } from "./components/UserClassCard";
 import NameCounter from "./components/NameCounter";
 import { NameCounterClass } from "./components/NameCounterClass";
+import TextareaComp from "./components/TextareaComp";
 
 function App() {
   console.log(user);
   return (
     <div className="App">
-      <UserCard
-        name={user.name}
-        age={user.age}
-        phoneNumber={user.phoneNumber}
-        address={user.address}
-      />
-
-      <br />
-
-      <UserClassCard
-        name={user.name}
-        age={user.age}
-        phoneNumber={user.phoneNumber}
-        address={user.address}
-      />
-
-      <br />
-
-      <NameCounter />
-      <br />
-
-      <NameCounterClass />
+      <TextareaComp />
     </div>
   );
 }
